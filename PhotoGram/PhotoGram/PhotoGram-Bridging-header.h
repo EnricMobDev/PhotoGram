7127c02c22073bfd8be48fb57af34967cb69a9fd
Enric Pou Villanueva <enric.pou.villanueva@everis.com>
2018-08-13 22:29:01 +0200

    ADD the bridge because this library is in Objective-C

---
+++ PhotoGram/PhotoGram/PhotoGram-Bridging-header.h
@@ -0,0 +1,13 @@
+//
+//  PhotoGram-Bridging-header.h
+//  PhotoGram
+//
+//  Created by Enric Pou Villanueva on 13/8/18.
+//  Copyright © 2018 Enric Pou Villanueva. All rights reserved.
+//
+
+#ifndef PhotoGram_Bridging_header_h
+#define PhotoGram_Bridging_header_h
+
+
+#endif /* PhotoGram_Bridging_header_h */
