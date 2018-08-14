973e9cc2024d7653abcf8af0daf0c56bb6f350ad
Enric Pou Villanueva <enric.pou.villanueva@everis.com>
2018-08-14 20:52:20 +0200

    ADD menu cell drawer

--- PhotoGram/PhotoGram/Drawers/MenuCellDrawer.swift
+++ PhotoGram/PhotoGram/Drawers/MenuCellDrawer.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import UIKit
+
+internal final class MenuCellDrawer: CellDrawerProtocol {
+    
+    // MARK: - Constants
+    private struct Constants {
+        static let reuseID = "MenuCell"
+    }
+    
+    // MARK: - CellDrawerProtocol
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return tableView.dequeueReusableCell(withIdentifier: Constants.reuseID,
+                                             for: indexPath)
+    }
+    
+    func drawCell(_ cell: UITableViewCell, withItem item: Any) {
+        
+        guard let cell = cell as? MenuTableViewCell,
+            let item = item as? OptionsMenu else {
+                return
+        }
+        
+        cell.selectionStyle = .none
+        cell.menuImage.layer.cornerRadius = 10
+        cell.menuImage.image = item.image
+        cell.menuTitle.text = item.title
+    }
+}
+
+// MARK: - OptionsMenu + DrawerItem
+extension OptionsMenu: DrawerItemProtocol {
+    
+    var cellDrawer: CellDrawerProtocol {
+        
+        return MenuCellDrawer()
+    }
+}
