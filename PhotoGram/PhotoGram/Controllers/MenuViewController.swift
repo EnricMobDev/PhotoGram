ec7bd6cd3b0e501b996bbcc581640760549667d3
Enric Pou Villanueva <enric.pou.villanueva@everis.com>
2018-08-14 20:52:44 +0200

    UPDATE : Create the view controller with the drawer to show menu

--- PhotoGram/PhotoGram/Controllers/MenuViewController.swift
+++ PhotoGram/PhotoGram/Controllers/MenuViewController.swift
@@ -8,28 +8,44 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //MARK: - Constants
+    let createCells: [DrawerItemProtocol] = [
+        
+        OptionsMenu(image: UIImage(named: "profile_image_male") ?? UIImage(), title: "Logout"),
+        OptionsMenu(image: UIImage(named: "logout") ?? UIImage(), title: "Logout")
+    ]
+    
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Creation of table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return createCells.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let item = createCells[indexPath.row]
+        let drawer = item.cellDrawer
+        
+        let cell = drawer.tableView(tableView, cellForRowAt: indexPath)
+        drawer.drawCell(cell, withItem: item)
+        
+        return cell
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = CellComponents.estimatedRowHeigth
+        return tableView.rowHeight
+    }
 }
